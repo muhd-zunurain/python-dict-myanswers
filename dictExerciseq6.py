9b18ca3c436d48c8c1131d22e21da7fe8a072594
Muhd Zunurain <zunurain.zulkifle@outlook.com>
2022-08-06 23:58:02 +0800

    Answered final question!!

---
+++ dictExerciseq6.py
@@ -0,0 +1,96 @@
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+# Write a function that takes a category and computes 
+# the average IMDB score (HINT: reuse the function 
+# from question 2.)
+
+def averageBasedOnCategory(movies, category):
+    sum = 0
+    count = 0
+    for key in movies:
+        if key['category'] == category:
+            count += 1
+            sum += key['imdb']
+    average = sum/count
+    print(f"IMDB SUM IS {sum}")
+    return (average, count)
+
+getCategory = input("Enter your desired category and I will compute the average: ")
+result = averageBasedOnCategory(movies, getCategory)
+print(f'For {getCategory} type of movies, the average imdb ratings is {result[0]}. There are {result[1]} movies under this category.')
