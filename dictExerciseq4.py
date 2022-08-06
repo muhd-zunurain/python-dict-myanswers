7c719cb5eccda4b9bbad0d5d9055948b16ee6c3a
Muhd Zunurain <zunurain.zulkifle@outlook.com>
2022-08-06 23:39:30 +0800

    Answered Question 4

---
+++ dictExerciseq4.py
@@ -0,0 +1,93 @@
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+# Write a function that takes a category name and returns 
+# just those movies under that category.
+def movieCategory(movies, catName):
+    outList = []
+    for key in movies:
+        if catName == key['category']:
+            outList.append(key)
+        else:
+            continue
+    return outList
+catName = input("Enter Category Name: ")
+
+out_list=movieCategory(movies, catName)
+print(f'Movies under {catName} category is as follows:')
+for key in out_list:
+    print(key['name'])
